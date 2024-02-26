cb3a58736664ccab29d145c67215c8d2b1f69ae3
Ryan Barfield <cadium828@gmail.com>
2024-02-25 18:47:03 -0600

    updated searchView to accept search requests and dispatch to reducer

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { requestGiphySearch } from '../Actions/Actions';
 import './SearchView.css';
 
 function SearchView() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault;
+    dispatch(requestGiphySearch(searchTerm));
+  };
+
   return (
     <>
       <h1 className="search-header">GIPHY SEARCH</h1>
       <div className="main-container">
-        <div className="search-container">
-          <input type="text" className="search-input" placeholder="Search..." />
-        </div>
-        <button className="search-button">Search</button>
+        <form onSubmit={handleSubmit}>
+          <div className="search-container">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        </form>
       </div>
     </>
   );
