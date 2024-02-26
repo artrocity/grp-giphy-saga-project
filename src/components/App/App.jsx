98a24d7fdd08ff3fa77d05a33c0a0a6ce5e715de
Richard Barnitz <richardbarnitz@gmail.com>
2024-02-25 16:58:23 -0800

    added routes and favoritesview

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import SearchView from '../SearchView/SearchView';
+import FavoritesView from '../FavoritesView/FavoritesView';
 
 function App() {
   return (
     <div>
-      <SearchView />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <SearchView />
+          </Route>
+
+          <Route path="/favorites" exact>
+            <FavoritesView />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/FavoritesView/FavoritesView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FavoritesView() {
+  return (
+    <>
+      <h1>YOUR FAVORITES</h1>
+      <div>
+        <input type="text" className="search-input" placeholder="Search..." />
+      </div>
+    </>
+  );
+}
+
+export default FavoritesView;
