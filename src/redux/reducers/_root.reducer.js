9c72be0c04d77b7f24045a6be298aaae71cd7d12
Ryan Barfield <cadium828@gmail.com>
2024-02-25 18:29:36 -0600

    updated reducers folder for initial reducers

---
+++ src/redux/reducers/_root.reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// Import Reducers
+
+// Combine Reducers
+const rootReducer = combineReducers({
+  // reducer1
+  // reducer2
+});
+
+export default rootReducer;
